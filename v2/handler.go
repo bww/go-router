657ad539fb2ee1b3dc1d07b07035475223db4e3c
Brian W. Wolter <brianwolter@gmail.com>
2024-06-01 10:03:15 -0400

    ...

--- v2/handler.go
+++ v2/handler.go
@@ -8,9 +8,22 @@ type Middle interface {
 	Wrap(Handler) Handler
 }
 
-// Middleware function wrapper
+// Middle function wrapper
 type MiddleFunc func(Handler) Handler
 
+// MiddleFunc conforms to Middle by calling itself with the handler
 func (m MiddleFunc) Wrap(h Handler) Handler {
 	return m(h)
+}
+
+// A set of middleware
+type Middles []Middle
+
+// Middles is itself conforms to Middle and wraps a handler with all
+// of its sub-middleware
+func (m Middles) Wrap(h Handler) Handler {
+	for _, e := range m {
+		h = e.Wrap(h)
+	}
+	return h
 }
